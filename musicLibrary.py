3ab8b43298b8c1c2d3c90112900490c1b2879fbb
Kirti <kirti041999@gmail.com>
2025-07-24 15:57:17 +0530

    initial commit

---
+++ musicLibrary.py
@@ -0,0 +1,5 @@
+music ={
+    "maula":"https://youtu.be/l5sgIqzlPXc?si=Hj6D4PUKmCu1EZF6",
+    "eastside":"https://youtu.be/56WBK4ZK_cw?si=SGfa7TLYFOUDPyW5",
+    "ghost":"https://youtu.be/ao4o-XRU_KM?si=gaAgE4MTUFHKG5bq"
+}
